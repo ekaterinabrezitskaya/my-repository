7690102ce722676119d0887d395c07db626eac8d
ekaterinabrezitskaya <39058808+ekaterinabrezitskaya@users.noreply.github.com>
2018-05-08 10:24:08 +0300

    Delete qwer2.py

--- qwer2.py
+++
@@ -1,5 +0,0 @@
-Dishes={'1':'Soup','2':'Main dish','3':'Salad'}
-
-Dish = input("\nWhat do you want on the dinner?\n \n 1.Soup \n 2.Main dish \n 3.Salad \n \nPlease, choose one number of dish from variants:")
-Meat = input("\n" + Dishes[Dish] +" "+ "must be with chicken, pork or beef?:") 
-
